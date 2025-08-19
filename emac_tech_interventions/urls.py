1a3f99c41f5efa5f5489f472a1c72aa4dec0cf7a
ezekiel342007 <ezekiel342007@gmail.com>
2025-08-19 16:44:17 +0100

    worked on http-only cookie for refresh token

--- emac_tech_interventions/urls.py
+++ emac_tech_interventions/urls.py
@@ -18,13 +18,14 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
     TokenRefreshView,
 )
+
+from intervention_blog.views import CookieTokenObtainPairView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include("intervention_blog.urls")),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CookieTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- intervention_blog/views.py
+++ intervention_blog/views.py
@@ -1,14 +1,17 @@
+from datetime import datetime, timedelta, timezone
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics 
+from rest_framework.request import Request 
 from rest_framework.pagination import LimitOffsetPagination
-from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from rest_framework.response import Response
 from rest_framework.status import HTTP_201_CREATED
 from rest_framework_simplejwt import authentication
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 from intervention_blog.filters import BlogFilter
 
-from .serializers import BlogSerializer, UserRegistrationSerializer
+from .serializers import BlogSerializer, UserProfileSerializer, UserRegistrationSerializer
 from .models import Blog, UserProfile
 
 # Create your views here.
@@ -57,3 +60,34 @@ class Users(generics.CreateAPIView):
             headers=headers,
             status=HTTP_201_CREATED
         )
+
+
+class CurrentUser(generics.RetrieveAPIView):
+    serializer_class = UserProfileSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
+
+
+class CookieTokenObtainPairView(TokenObtainPairView):
+    def post(self, request: Request, *args, **kwargs) -> Response:
+        response = super().post(request, *args, **kwargs)
+
+        if response.status_code == 200:
+            refresh_token = response.data.get("refresh")
+
+            response.set_cookie(
+                key="refresh_token",
+                value=refresh_token,
+                httponly=True,
+                secure=True,
+                samesite="Lax",
+                expires=datetime.now(timezone.utc) + timedelta(days=7)
+            )
+
+            response.data.pop("refresh")
+
+        return response
+
+
